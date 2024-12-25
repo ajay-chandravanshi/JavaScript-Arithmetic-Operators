9d1e052d3ceafa17d35eb119e6f7f9f7464f4b7b
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-25 23:44:16 +0530

    new code

---
+++ 8.All in one operator arithmetic.js
@@ -0,0 +1,24 @@
+let q=14;
+let r=16;
+var s=q+r;
+console.log(s);
+
+var s=q-r;
+console.log(s);
+
+var s=q*r;
+console.log(s);
+
+var s=q/r;
+console.log(s);
+
+var s=q%r;
+console.log(s);
+
+var s=q+r;
+s++;
+console.log(s);
+
+var s=q+r;
+s--;
+console.log(s);
